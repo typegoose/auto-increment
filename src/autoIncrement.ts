41d8f7f22d60c84253fc0e972b9b732ba5cc7a84
hasezoey <hasezoey@gmail.com>
2020-10-17 20:54:38 +0200

    fix(autoIncrement): remove deprecated "isNullOrUndefined"

--- src/autoIncrement.ts
+++ src/autoIncrement.ts
@@ -1,9 +1,16 @@
 import * as mongoose from 'mongoose';
-import { isNullOrUndefined } from 'util';
 import { logger } from './logSettings';
 import { AutoIncrementIDOptions, AutoIncrementIDTrackerSpec, AutoIncrementOptionsSimple } from './types';
 
 const DEFAULT_INCREMENT = 1;
+
+/**
+ * Because since node 4.0.0 the internal util.is* functions got deprecated
+ * @param val Any value to test if null or undefined
+ */
+export function isNullOrUndefined(val: unknown): val is null | undefined {
+  return val === null || val === undefined;
+}
 
 /**
  * The Plugin - Simple version
