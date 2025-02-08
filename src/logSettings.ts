db321bd1aa0bef7fd3fbfa89615037e87e94e0be
hasezoey <hasezoey@gmail.com>
2025-02-08 14:12:01 +0100

    fix(logSettings): actually allow more levels than "warn" and "error"
    "loglevel" is self-modifying, requiring getter functions to get the correct function at the time.

--- src/logSettings.ts
+++ src/logSettings.ts
@@ -2,12 +2,14 @@ import * as _logger from 'loglevel';
 
 // this has to be re-defined as otherwise jest is not able to mock any function
 // when "esModuleInterop: true" and "_logger" is not default imported
-// and "_logger" cannot be default imported as that would make all projects using auto-increment require to set "allowSyntheticDefaultImports"
+// and "_logger" cannot be default imported as that would make all projects using typegoose require to set "allowSyntheticDefaultImports"
 export const logger = {
-  warn: _logger.warn,
-  error: _logger.error,
-  info: _logger.info,
-  debug: _logger.debug,
+  // "loglevel" here referred as "_logger" is self-modifying, so a getter function is necessary to actually get logs
+  // otherwise we only get the default "noop" function forever
+  warn: (...args) => _logger.warn.apply(undefined, args),
+  error: (...args) => _logger.error.apply(undefined, args),
+  info: (...args) => _logger.info.apply(undefined, args),
+  debug: (...args) => _logger.debug.apply(undefined, args),
   setLogLevel: _logger.setLevel,
   LogLevels: _logger.levels,
   setDefaultLevel: _logger.setDefaultLevel,
